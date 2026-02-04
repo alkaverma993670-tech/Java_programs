ed93885e4f2179a26401edaf25e3203a327b08ac
Alka Verma <alkaverma993670@gmail.com>
2026-02-04 09:05:35 +0530

    Add files via upload

---
+++ gretest.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class largest {
+public static void main(String arg[])
+{
+	Scanner sc= new Scanner(System.in);
+	System.out.println("enter the value of a:");
+	int a= sc.nextInt();
+	System.out.println("enter the value of b:");
+	int b= sc.nextInt();
+	if(a>b)
+	System.out.println("a is largest");
+	else
+	System.out.println("b is largest");
+	
+}
+}
+
+
+
