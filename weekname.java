e985fa8b70f12d3cbb3d6447104ae661c11f11bf
Alka Verma <alkaverma993670@gmail.com>
2026-02-04 14:10:08 +0530

    Add files via upload

---
+++ weekname.java
@@ -0,0 +1,17 @@
+class Test3{
+public static void main(String args[])
+
+{
+	int day =3;
+	switch(day){
+	case 1: System.out.println("monday");
+	case 2: System.out.println("tuesday");
+	case 3: System.out.println("wednessday");
+	case 4: System.out.println("thursday");
+	case 5: System.out.println("friday");
+	case 6: System.out.println("satuday");
+	case 7: System.out.println("sunday");
+
+		
+}
+}		
