5eae5566f179a74b55ab25a9f32c4832ea80f937
Alka Verma <alkaverma993670@gmail.com>
2026-02-04 09:14:32 +0530

    Add files via upload

---
+++ month.java
@@ -0,0 +1,31 @@
+class DayMonth {
+public static void main(String args[])
+{
+	
+	int month=2 , days;
+	
+	switch(month){
+		case 1: case 3: case 5: case 7: case 8: case 10: case 12:
+	days=31;
+	break;
+
+	case 4: case 6: case 9: case 11:
+
+	days=30;
+	break;
+
+	case 2:
+
+	days=28;
+	break;
+	default : System.out.println("invalid number");
+
+	}
+	System.out.println(" number of days in month "  + month + "=" + days) ;
+		
+
+
+	
+}
+}
+
